17f7f8aef74021aee8a465306de70ee7e4216960
Saahil <neon@saahild.com>
2024-09-08 18:53:08 -0400

    chore(ci): quick update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import "dotenv/config";
-import "./modules/watch-git";
+import "dotenv/config"
+import "./modules/watch-git"
 // import "./modules/smee"
-import app from "./modules/slackapp";
+import app from './modules/slackapp'
 
 app.start(process.env.PORT || 3000).then((d) => {
-  console.log(`App is UP (please help)`);
-});
+    console.log(`App is UP (please help)`)
+})
 // this is temp i swear
-<<<<<<< HEAD
 app.command('/ping',async ({ command, ack, respond }) => {
     const stamp = Date.now()
     await ack()
@@ -32,46 +31,21 @@ app.event('app_home_opened', async ({ event, client, logger }) => {
                 type: "mrkdwn",
                 text: "*Welcome home, <@" + event.user + "> :house:*"
               }
-=======
-app.command("/ping", async ({ command, ack, respond }) => {
-  const stamp = Date.now();
-  await ack();
-  respond(`Pong took: \`${Date.now() - stamp}ms\``).then((d) => {
-    console.log(d);
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+              }
+            }
+          ]
+        }
+      });
+  
+      logger.info(result);
+    }
+    catch (error) {
+      logger.error(error);
+    }
   });
-});
-// Listen for users opening your App Home
-app.event("app_home_opened", async ({ event, client, logger }) => {
-  try {
-    // Call views.publish with the built-in client
-    const result = await client.views.publish({
-      // Use the user ID associated with the event
-      user_id: event.user,
-      view: {
-        // Home tabs must be enabled in your app configuration page under "App Home"
-        type: "home",
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "*Welcome home, <@" + event.user + "> :house:*",
->>>>>>> refs/remotes/origin/main
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.",
-            },
-          },
-        ],
-      },
-    });
-
-    logger.info(result);
-  } catch (error) {
-    logger.error(error);
-  }
-});
