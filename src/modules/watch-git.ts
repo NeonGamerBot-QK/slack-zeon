bb3c3bf357ab7a64c273c1833487253fe77f3909
Saahil <saahilattud@gmail.com>
2024-09-25 15:11:19 -0400

    feat: eval cmd & git watcher

--- src/modules/watch-git.ts
+++ src/modules/watch-git.ts
@@ -5,16 +5,23 @@ export default function (app: any) {
       let response = error?.stdout ? error?.stdout[0].toString() : stdout;
       if (!error) {
         if (!response?.includes("Already up to date.")) {
+          const commitMessage = require("child_process")
+          .execSync("git log -1 --pretty=%B")
+            .toString();
           console.log(`New git stuff wowie`);
           console.log(response);
           app.client.chat.postMessage({
             channel: `D07LBMXD9FF`,
-            text: `\`\`\`\n${response.slice(0, 4000)}\`\`\``,
+            text: `**${commitMessage}**\`\`\`\n${response.slice(0, 4000)}\`\`\``,
           });
-          app.client.chat.postMessage({
-            channel: `C0P5NE354`,
-            text: `\`\`\`\n${response.slice(0, 4000)}\`\`\``,
-          });
+         
+        console.debug(commitMessage)
+          if (commitMessage.startsWith('feat')) {
+            app.client.chat.postMessage({
+              channel: `C0P5NE354`,
+              text: `\`\`\`\n${response.slice(0, 4000)}\`\`\``,
+            });
+     }
           setTimeout(() => {
             process.exit();
           }, 1000);
