2151226ce013663715ce3f4c2911d8f73eebacae
Saahil <saahilattud@gmail.com>
2024-10-10 11:16:47 -0400

    feat: channel create event

---
+++ src/commands/on_channel.ts
@@ -0,0 +1,24 @@
+// 
+
+import { App } from "@slack/bolt";
+import { Command, onlyForMe } from "../modules/BaseCommand";
+
+export default class AutoJoinChannel implements Command {
+  name: string;
+  description: string;
+  is_event?: boolean;
+  constructor() {
+    this.name = `channel_created`;
+    this.description = `Channel creation - auto join`;
+    this.is_event = true;
+  }
+  run(app: App) {
+    // app.command()
+    app.event(this.name, async ({ event, say }) => {
+      console.debug(event, "#channel_create");
+        app.client.conversations.join({
+            channel: event.channel.id
+      })
+    });
+  }
+}
