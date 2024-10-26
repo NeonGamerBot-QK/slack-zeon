e610e6806fba593f6b107e8528ae18e1a9ef4f1c
Saahil <saahilattud@gmail.com>
2024-10-26 17:52:31 -0400

    chore: add it to invite user.

--- src/modules/slackapp.ts
+++ src/modules/slackapp.ts
@@ -94,7 +94,17 @@ export const app = new App({
                       thread_ts: d.ts,
                       text: `:thread: Responses about new song here please!`,
                     })
-                    .then(() => {
+                    .then(async () => {
+                      // try to invite user
+                      try {
+  await   app.client.conversations.invite({
+    channel: "C07RE4N7S4B",
+    //@ts-ignore
+    users: req.body.user
+  });
+                      } catch (e) {
+                        console.error(`Failed to invite user.`)
+}
                       //@ts-ignore
                       if (!req.body.noping) {
                         app.client.chat.postMessage({
