63354d6ba2cfd6b1942ce1896c779cdddd53276b
Saahil <neon@saahild.com>
2024-10-31 20:02:21 -0400

    update: finish up

--- src/modules/nextdns.ts
+++ src/modules/nextdns.ts
@@ -2,11 +2,11 @@ import { ModifiedApp } from "./slackapp";
 import EventSource from "eventsource";
 export interface Root {
   timestamp: string;
-  domain: string;
+  domain?: string;
   root: string;
   encrypted: boolean;
   protocol: string;
-  clientIp: string;
+  clientIp?: string;
   device: Device;
   status: string;
   reasons: Reason[];
@@ -25,6 +25,17 @@ export function PrivateDNS(app: ModifiedApp, id: string, channel: string) {
   const client = new EventSource(`https://api.nextdns.io/profiles/${id}/logs/stream`, {
     headers: { "X-Api-Key": process.env.NEXTDNS_API_KEY },
   });
+  client.onmessage = message => {
+    const realData: Root = JSON.parse(message.data);
+        console.log(realData);
+        //@ts-ignore
+        delete realData.clientIp;
+        //   console.log(`${realData.status == 'blocked' ? ':x:' : ":white_check_mark:"} ${realData.encrypted ? ":lock: " : ""} - ${realData.domain} `)
+        app.client.chat.postMessage({
+          channel,
+          text: `${realData.status == "blocked" ? ":x:" : ":white_check_mark:"} ${realData.encrypted ? ":lock: " : ""} - ${realData.domain} `,
+        });
+  }
   // fetch(`https://api.nextdns.io/profiles/${id}/logs/stream`, {
   //   headers: { "X-Api-Key": process.env.NEXTDNS_API_KEY },
   // }).then((r) => {
