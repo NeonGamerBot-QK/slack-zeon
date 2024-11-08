a8b9a7322306bd1529238b642e44cdeec0115243
Saahil <saahilattud@gmail.com>
2024-11-07 22:18:10 -0500

    qf: codewatcher

--- src/commands/codewatcher.ts
+++ src/commands/codewatcher.ts
@@ -16,19 +16,31 @@ export default class codewatcher implements Command {
 
       if (!onlyForMe(command.user_id))
         return respond(`:x: You cannot use this command.`);
-
-      const subcmd = command.text.split(" ")[1];
+const args = command.text.split(" ");
+      const subcmd = args.shift()
       switch (subcmd) {
         case "start":
+          const reponame = args[0]
           app.client.chat.postMessage({
             channel: command.channel_id,
-            text: `Starting Code Watcher`,
+            text: `Starting Code Watcher for ${reponame}`,
           });
+
           break;
         case "stop":
           let d = app.db.get("git_session") || [];
+          if (!d[d.indexOf(d.find((e) => e.active))]) {
+            app.client.postEphemeral({
+              channel: command.channel_id,
+              user: command.user_id,
+              text: `No session atm`,
+          })
+            return;
+          }
           // d = d.filter(e=>e.active);
+          d[d.indexOf(d.find((e) => e.active))].ended_at = Date.now() 
           d[d.indexOf(d.find((e) => e.active))].active = false;
+          
           app.db.set("git_session", d);
           app.client.chat.postEphemeral({
             channel: command.channel_id,
