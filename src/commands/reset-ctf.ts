422f239abb3690ee865821290408fde308bbcd7c
Saahil <neon@saahild.com>
2024-11-11 21:39:30 -0500

    feat(cmd): maybe reset system?!

---
+++ src/commands/reset-ctf.ts
@@ -0,0 +1,116 @@
+import { App } from "@slack/bolt";
+import { Command, onlyForMe } from "../modules/BaseCommand";
+import { ModifiedApp } from "../modules/slackapp";
+import path from "path";
+import fs from "fs";
+import StegCloak from "stegcloak";
+
+const stegcloak = new StegCloak(true, false);
+
+function decrypt(str) {
+  return stegcloak.reveal(str, process.env.CTF_PASSWORD);
+}
+const files = fs.readdirSync(path.join(__dirname, "../../ctf", "notes"));
+
+function parseTheSecrets() {
+    return new Promise((res) => {
+        let i = 0;
+        let total = []
+        for(const file of files) {
+            const data = fs.readFileSync(path.join(__dirname, "../../ctf", file)).toString();
+            total.push(JSON.parse(decrypt(data)));
+            if (i == files.length - 1) {
+              console.log("Done!");
+res(total)
+            }
+            i++;
+        }
+    })
+}
+export function createChannel(app:ModifiedApp, name: string, userIds?: string[]) {
+    return  app.client.conversations.create({
+        name: name,
+        is_private: true
+        }).then((d)=>{
+        // console.log(d)
+        app.client.conversations.invite({
+        channel: d.channel.id,
+        users: `${process.env.MY_USER_ID}${userIds.length > 0 ? "," : ""}${userIds?.join(",")}`
+        })
+        // setTimeout(() => {
+        // app.client.conversations.archive({
+        // channel: d.channel.id
+        // })
+        // }, 120 * 1000)
+        return d;
+        })
+}
+export default class Ping implements Command {
+  name: string;
+  description: string;
+  constructor() {
+    this.name = `/reset-ctf`;
+    this.description = `Resets the CTF`;
+  }
+  run(app: ModifiedApp) {
+    // app.command()
+    app.command(this.name, async ({ command, ack, respond }) => {
+      await ack();
+
+      if (!onlyForMe(command.user_id))
+        return respond(`:x: You cannot use this command.`);
+
+      // respond(`Pong took: \`${Date.now() - stamp}ms\``).then((d) => {
+      //   console.debug(`after ping`, d);
+      // });
+      const msg = await app.client.chat
+      .postMessage({
+        text: `Resetting CTF`,
+        channel: command.channel_id,
+      })
+// app.db.delete("ctf")
+const currentDbInstance = app.db.get("ctf");
+if(!currentDbInstance) {
+    console.log(`First run! hopefully i dont fuck it up`)
+    let compiledJSON:any = await parseTheSecrets();
+    // insert this into the db? why: A: easier to add stuff, B: wawa idc about the db having unencrypted data
+    app.db.set("ctf", compiledJSON); 
+
+    for(let j of compiledJSON) {
+        if(j.bin_content) {
+    // upload to hastebin
+  await   fetch(`https://bin.saahild.com/documents`, {
+        method: "POST",
+        body: j.bin_content
+    }).then(r=>r.json()).then(hd => {
+        j.bin_id = hd.key;
+        // app.db.set("ctf", compiledJSON);
+    })
+        }
+        if(j.create_channel) {
+let n = null;
+if(j.random_channel_name) n = Date.now().toString()
+if(j.channel_name_be_bin) n = j.bin_id
+const ch = await createChannel(app, n);
+j.ch_id = ch.channel.id;
+if(j.channel_message) {
+await    app.client.chat.postMessage({
+        channel: ch.channel.id,
+        text: j.channel_message
+    })
+}
+if(j["2nd_message"]) {
+    await    app.client.chat.postMessage({
+        channel: ch.channel.id,
+        text: j["2nd_message"]!
+    })
+}
+
+}
+    }
+    app.db.set("ctf", compiledJSON); 
+
+}
+    });
+  }
+}
