2ca95dc873297055f0bd229a796bc5fda58ed492
Saahil <neon@saahild.com>
2024-11-21 00:10:27 -0500

    update: channels and messages

--- src/modules/randomResponseSystem.ts
+++ src/modules/randomResponseSystem.ts
@@ -1,6 +1,6 @@
 import JSONdb from "simple-json-db";
 
-export const acRandom = [
+export const  acRandom = () =>  ([
   "It might be {hour} but i COULD be tweaking",
   "hello today, my name is ~markiplier~ zeon.",
   "Wana break from the ads, tap now because YOU can get spotify premium",
@@ -13,38 +13,36 @@ export const acRandom = [
   "Blahaj :blahaj2: ",
   "Blahaj (spinny) :blahaj-spin: ",
   ":notcool::notcool::notcool:\n> https://hackclub.slack.com/archives/C07FDHUL50F/p1728928832148439",
-];
+])
 // used to adverties channels
 export const beggingMessage = [
   "PLEASE JOIN IT RAA",
   "Its epik i swear",
   "warning: may contain extremly dead chat.",
 ];
-// if you want to become a neighboor of this channel just dm me on slack
-export const neighbors = [
-  "C06R5NKVCG5",
-  "C04H0MG1BLN",
-  "C07SLT702UA",
-  "C027Y33B93L",
-  "C0793T42XV4",
-  "C07S1QSSKTQ",
-  "C07MEM8S0LE",
-];
+// if you want to become a neighboor of this channel just dm me on slack or add your self to the canvas
+export const neighbors = ["C06R5NKVCG5","C04H0MG1BLN","C07SLT702UA","C074L3A4C3E","C0793T42XV4","C07S1QSSKTQ","C07RQ6682N8","C080NCDAVNH","C064DNF64LU"]
 // dont dupe channels :P
 export const channelsToAdvs = [
   ...new Set([
     ...neighbors,
-    "C07QWGLQUH2",
-    "C07RE4N7S4B",
-    "C07PGEGJ3B6",
-    "C07LZ237WCF",
-    "C07LEEB50KD",
-    "C07STMAUMTK",
-    "C07RW1666UV",
+    "C07QWGLQUH2", // kcd-lunch
+    "C07RE4N7S4B",// hackclub-spotify-bot
+    "C07PGEGJ3B6", // hackclub-verification
+    "C07LZ237WCF", // nest-pgp-chat
+    "C07LEEB50KD", // zeon-public
+    "C07STMAUMTK", //hackclub-ai-faq-bot
+    "C07RW1666UV", //zeon-bdays
+    "C080G5TM3A4", // capture-the-flag
+    "C080T3WUTK4", // discord-dataming
+    "C07UNAHD9C3", // stickers-watcher
+    "C07TKPC0ZNZ", // surakku
+    "C07V6F1A5FH",  // web bridge slack
+    "C07MLF9A8H5", // riceathon
   ]),
 ];
 export function actualRandomResponse() {
-  return parseRandom(acRandom[Math.floor(Math.random() * acRandom.length)]);
+  return parseRandom(acRandom()[Math.floor(Math.random() * acRandom().length)]);
 }
 export function parseRandom(str: string): string {
   Object.entries(process.env)
@@ -83,12 +81,12 @@ export async function checkOverSpending(db: JSONdb) {
   if (currentTransactions.length > 0) {
     const firstTransaction = currentTransactions[0];
     db.set("overspending_index", sliceIndex + 1);
-    return `Wow, you have spent so much money today, (${firstTransaction.amount}) (${Math.round(Math.random()) ? "fatass" : "bigback"}-`;
+    return `Wow, you have spent so much money today, (${firstTransaction.amount}) (${Math.round(Math.random()) ? "fatass" : "bigback"}-)`;
   }
   return false;
 }
 export async function getResponse(db: JSONdb): Promise<string> {
-  let chanceOfChannelAdvs = isItMyChance();
+  let chanceOfChannelAdvs = isItMyChance(20);
   const overSpending = await checkOverSpending(db);
   if (overSpending) return overSpending;
   if (chanceOfChannelAdvs && last_type !== ResponseTypes.ChannelAdvs) {
