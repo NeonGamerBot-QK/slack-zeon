4bb3056ddbe85fce18cf471e418e9673428b0015
Saahil <neon@saahild.com>
2024-12-10 22:59:54 -0500

    feat: bday

--- src/commands/bdays.ts
+++ src/commands/bdays.ts
@@ -2,7 +2,7 @@ import { App } from "@slack/bolt";
 import { Command, onlyForMe } from "../modules/BaseCommand";
 import { ModifiedApp } from "../modules/slackapp";
 
-export default class Ping implements Command {
+export default class Bday implements Command {
   name: string;
   description: string;
   constructor() {
@@ -12,20 +12,34 @@ export default class Ping implements Command {
   run(app: ModifiedApp) {
     // app.command()
     app.command(this.name, async ({ command, ack, respond }) => {
-      const stamp = Date.now();
       await ack();
-
       // first make it store them
       // TODO
       const args = command.text.split(/ +/);
       const cmd = args.shift().toLowerCase();
       if (cmd == "config") {
-        await app.client.chat.postMessage({
+        try {
+          const formatedDate = new Date(args.join(" "));
+          app.dbs.bday.set(command.user_id, formatedDate.toISOString());
+          await app.client.chat.postEphemeral({
+            channel: command.channel_id,
+            user: command.user_id,
+            text: `:white_check_mark: Set your bday to ${formatedDate.toDateString()}`,
+          })
+        } catch (e) {
+        await app.client.chat.postEphemeral({
           channel: command.channel_id,
-          text: `:x: not done yet ...`,
+          user: command.user_id,
+          text: `:x: Please use a valid date (eg: \`YYYY-MM-DD\` or \`1733888655020\`) (or i just broke)\n> ${e.message}`,
         });
+      }
       } else if (cmd == "remove-my-data") {
         app.dbs.bday.delete(command.user_id);
+        await app.client.chat.postEphemeral({
+          channel: command.channel_id,
+          user: command.user_id,
+          text: `:white_check_mark: Removed your bday data`,
+        });
       }
     });
   }
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -12,3 +12,4 @@ export * as hacktime from "./hacktime";
 export * as adventOfCode from "./adventofcode";
 export * as hcshipments from "./parseShipments";
 export * as hackclubcdn from "./hackclubcdn";
+export * as bdayutils from "./bday";
