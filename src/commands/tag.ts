4c0de0bd16d5314135d2bbaf5be3d0619f673e9d
Saahil <neon@saahild.com>
2024-12-13 18:14:53 -0500

    update: before the hard part

--- src/commands/tag.ts
+++ src/commands/tag.ts
@@ -1,7 +1,17 @@
 //Global tags for everyone
 import { App } from "@slack/bolt";
 import { Command, onlyForMe } from "../modules/BaseCommand";
-
+import { ModifiedApp } from "../modules/slackapp";
+export const tagstore = {
+// ONLY STATIC ONES, may use blocks
+  "testing": [{
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "testing",
+    }
+  }]
+}
 export default class TagSystem implements Command {
   name: string;
   description: string;
@@ -9,10 +19,12 @@ export default class TagSystem implements Command {
     this.name = `/tag`;
     this.description = `Tag system`;
   }
-  run(app: App) {
+  run(app: ModifiedApp) {
     // app.command()
-    app.command(this.name, async ({ command, ack, respond }) => {
+    //@ts-ignore
+    app.command(this.name, async (par) => {
       const stamp = Date.now();
+const { command, ack, respond } = par;
       await ack();
 
       // respond(`Pong took: \`${Date.now() - stamp}ms\``).then((d) => {
@@ -21,17 +33,98 @@ export default class TagSystem implements Command {
       const args = command.text.split(" ");
       const cmd = args.shift();
       if (!cmd)
-        return app.client.chat.postEmpheral({
-          channel: command.channel_id,
-          user: command.user_id,
-          text: `:x: Please provide a command`,
+        return    respond({
+          response_type: 'ephemeral',
+          text: `Please provide a command`,
         });
+      
       // usage: /tag use <tag>
       // user based tag store
       if (cmd == "use") {
         const tagName = args[0];
         // check if the tag exists
-        const tag = app.dbs.tags.get(`${event.user}_${tagName}`);
+        const tag = app.dbs.tags.get(`${command.user_id}_${tagName}`);
+        if(tag) {
+        await respond({
+          token: command.user_id == process.env.MY_USER_ID ? process.env.MY_SLACK_TOKEN : undefined,
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: tag
+              }
+            },
+            {
+              // context block
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: `Tag ${tagName}`,
+                }
+              ]
+            }
+          ]
+        })
+        } else {
+          await respond({
+            text: `Tag \`${tagName}\` does not exist`,
+          });
+        }
+      } else if(cmd == "static") {
+        const tagName = args[0];
+        // check if the tag exists
+        const tag = tagstore[tagName];
+        if(tag) {
+          await respond({
+            token: command.user_id == process.env.MY_USER_ID ? process.env.MY_SLACK_TOKEN : undefined,
+            blocks: [...tag, {
+              // context block
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: `Tag ${tagName}`,
+                }
+              ]
+            } ] 
+          })
+        }
+      } 
+      else if(cmd == "help") {
+       respond({
+          response_type: 'ephemeral',
+          text: `Zeon Tag System Help\n\n/tag use <tag>\n/tag add\n/tag rm <tag>`,
+        });
+      } else if(cmd == "list") {
+        const tags = Object.keys(app.dbs.tags.JSON()).filter(e=>e.startsWith(command.user_id)).map(e=> {
+          return {
+            name: e.split("_").slice(1).join("_"),
+            value: app.dbs.tags.get(e),
+          }
+        });
+        await respond({
+          response_type: "ephemeral",
+          text:  `Tags:\n${tags.map((e,i)=>`${i+1}. ${e.name}`).join("\n")}`,
+        })
+      }
+       else if(cmd == "rm") {
+        const tagName = args[0];
+        // check if the tag exists
+        const tag = app.dbs.tags.get(`${command.user_id}_${tagName}`);
+        if(tag) {
+          app.dbs.tags.delete(`${command.user_id}_${tagName}`);
+          respond({
+            response_type: 'ephemeral',
+            text: `Tag ${tagName} removed`,
+          });
+        } else {
+          respond({
+            response_type: 'ephemeral',
+            text: `Tag \`${tagName}\` does not exist`,
+          });
+        }
       }
     });
     app.view("view_modal_tag", async ({ ack, body, view, context }) => {
