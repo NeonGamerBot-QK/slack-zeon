67d599936459277d2f2632048ee3352a60b59238
Saahil <neon@saahild.com>
2024-12-15 13:47:15 -0500

    feat: site

--- src/modules/slackapp.ts
+++ src/modules/slackapp.ts
@@ -13,6 +13,72 @@ export interface ModifiedApp extends App<StringIndexed> {
   ws: null | any;
   utils: typeof import("./index");
 }
+export function buildHtml() {
+  // oh yes im writing a whole ass website in ts file with no jsx
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Zeon slack </title>
+  <meta name="description" content="Zeon slack">
+  <meta name="author" content="NeonGamerBot-QK">
+
+  <style>
+  :root {
+    --rosewater: #f5e0dc;
+    --flamingo: #f2cdcd;
+    --pink: #f5c2e7;
+    --mauve: #cba6f7;
+    --red: #f38ba8;
+    --maroon: #eba0ac;
+    --peach: #fab387;
+    --yellow: #f9e2af;
+    --green: #a6e3a1;
+    --teal: #94e2d5;
+    --sky: #89dceb;
+    --sapphire: #74c7ec;
+    --blue: #89b4fa;
+    --lavender: #b4befe;
+    --text: #cdd6f4;
+    --subtext1: #bac2de;
+    --subtext0: #a6adc8;
+    --overlay2: #9399b2;
+    --overlay1: #7f849c;
+    --overlay0: #6c7086;
+    --surface2: #585b70;
+    --surface1: #45475a;
+    --surface0: #313244;
+    --base: #1e1e2e;
+    --mantle: #181825;
+    --crust: #11111b;
+    }
+  body,html {
+    color: var(--text);
+    background-color: var(--mantle);
+  }
+  a {
+  color: var(--mauve);
+  font-weight: bold;
+  }
+  .box {
+    color: var(--text);
+    border-radius: 0.5rem;
+    padding: 2px
+  }  
+  </style>
+</head>
+<body>
+<div class="box">
+<main><center>
+<h1> Zeon's site </h1>
+<p>wip, todo</p>
+</main></center>
+</div>
+</html>
+`
+}
 // Initializes your app with your bot token and signing secret
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -22,9 +88,11 @@ export const app = new App({
       path: "/",
       method: ["GET"],
       handler(req, res) {
-        res.setHeader(`Location`, `https://saahild.com`);
-        res.writeHead(302);
-        res.end(`bye`);
+        // res.setHeader(`Location`, `https://saahild.com`);
+        // res.writeHead(302);
+        // res.end(`bye`);
+        const site = buildHtml()
+        res.writeHead(200).end(site)
       },
     },
     {
