6c9c534c58071da65dcefb69aa2f5fedcc3b935e
Saahil <neon@saahild.com>
2024-12-17 17:36:07 -0500

    feat: watch tz

--- src/modules/watchMyIrl.ts
+++ src/modules/watchMyIrl.ts
@@ -1,24 +1,59 @@
 import { ModifiedApp } from "./slackapp";
 import cron from "node-cron";
+import {find as findTz} from "geo-tz"
 export default function watchLocation() {
   const location = null;
   // todo
 }
+export interface ShortcutUpdate {
+  battery: number;
+  clipboard: string;
+  focus: string
+  'weather ': string;
+  location: {
+    lat: number;
+    long: number;
+    city: string;
+    addr: string;
+    name: string;
+  }
 
-export async function watchBattery(app: ModifiedApp) {
-  const data = await fetch(
-    process.env.ZEON_DISCORD_INSTANCE + "/irl/shortcut_updates",
-    {
-      headers: {
-        Authorization: process.env.IRL_AUTH,
-      },
-    },
-  ).then((r) => r.json());
+}
+export interface IrlData {
+  latest_entry: ShortcutUpdate
+}
+const mainTimezone = "America/New_York";
+// ontimezoneswitch;
+export async function watchTimezone(app: ModifiedApp, data: IrlData) {
+  const tz = findTz(data.latest_entry.location.lat, data.latest_entry.location.long)[0]
+  if(tz !== mainTimezone) {
+   const m = await app.client.chat.postMessage({
+      text: `Hello yall! Neon's tz has changed to *${tz}*\n_replies from neon may now differ..._`,
+      channel: `C07R8DYAZMM`
+    })
+    app.db.set(`tz`, {
+      tz: tz,
+      m: m.ts
+    })
+  } else {
+    if(app.db.get(`tz`)) {
+      await app.client.chat.postMessage({
+        thread_ts: app.db.get(`tz`).m,
+        text: `Neon is back to his normal tz`,
+        channel: `C07R8DYAZMM`,
+        reply_broadcast: true
+      })
+      app.db.delete(`tz`)
+    }
+  }
+}
+export async function watchBattery(app: ModifiedApp, data: IrlData) {
+  
   const newBattery = data.latest_entry.battery;
   const lastEntry = app.db.get(`phone_battery`);
   if (newBattery != lastEntry && Math.abs(newBattery - lastEntry) > 50) {
     app.client.chat.postMessage({
-      channel: `C07UNAHD9C3`,
+      channel: `C07R8DYAZMM`,
       text: `Battery: *${newBattery}%* (changed more then 50%)`,
     });
   }
@@ -27,7 +62,7 @@ export async function watchBattery(app: ModifiedApp) {
     (app.db.get(`phone_bat_noti`) || 0) - Date.now() > 1000 * 60 * 10
   ) {
     app.client.chat.postMessage({
-      channel: `C07UNAHD9C3`,
+      channel: `C07R8DYAZMM`,
       text: `Hey your at %${newBattery}% battery, please charge your phone you idiot.`,
     });
     app.db.set(`phone_bat_noti`, Date.now());
@@ -37,6 +72,15 @@ export async function watchBattery(app: ModifiedApp) {
 
 export function setupCronForIrl(app: ModifiedApp) {
   cron.schedule("*/5 * * * *", async () => {
-    watchBattery(app);
+    const data = await fetch(
+      process.env.ZEON_DISCORD_INSTANCE + "/irl/shortcut_updates",
+      {
+        headers: {
+          Authorization: process.env.IRL_AUTH,
+        },
+      },
+    ).then((r) => r.json()) as IrlData;
+    watchBattery(app, data);
+    watchTimezone(app, data);
   });
 }
