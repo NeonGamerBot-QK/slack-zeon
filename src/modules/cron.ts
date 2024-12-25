71ddf0c0fc5de363734a040cc4d4f4bb24e50a98
Saahil <saahilattud@gmail.com>
2024-12-25 00:32:16 -0500

    feat: cron for airtable reverse

--- src/modules/cron.ts
+++ src/modules/cron.ts
@@ -165,6 +165,75 @@ export function setupOverallCron(app: ModifiedApp) {
         }
       });
   });
+  cron.schedule("*/5 * * * *", async () => {
+    try {
+      // idgaf about the temp creds
+      const temp1 = await fetch("https://airtable.com/v0.3/application/app05mIKwNPO2l1vT/readForSharedPages?stringifiedObjectParams=%7B%22includeDataForPageId%22%3A%22pagVJtLdaiiXaC2D1%22%2C%22shouldIncludeSchemaChecksum%22%3Atrue%2C%22expectedPageLayoutSchemaVersion%22%3A26%2C%22shouldPreloadQueries%22%3Atrue%2C%22shouldPreloadAllPossibleContainerElementQueries%22%3Atrue%2C%22urlSearch%22%3A%22%22%2C%22includeDataForExpandedRowPageFromQueryContainer%22%3Atrue%2C%22includeDataForAllReferencedExpandedRowPagesInLayout%22%3Atrue%2C%22navigationMode%22%3A%22view%22%7D&requestId=reqFv03qxbFJ1EvG3&accessPolicy=%7B%22allowedActions%22%3A%5B%7B%22modelClassName%22%3A%22page%22%2C%22modelIdSelector%22%3A%22pagVJtLdaiiXaC2D1%22%2C%22action%22%3A%22read%22%7D%2C%7B%22modelClassName%22%3A%22application%22%2C%22modelIdSelector%22%3A%22app05mIKwNPO2l1vT%22%2C%22action%22%3A%22readForSharedPages%22%7D%2C%7B%22modelClassName%22%3A%22application%22%2C%22modelIdSelector%22%3A%22app05mIKwNPO2l1vT%22%2C%22action%22%3A%22readSignedAttachmentUrls%22%7D%5D%2C%22shareId%22%3A%22shrGTZVv0GaS4S4Kk%22%2C%22applicationId%22%3A%22app05mIKwNPO2l1vT%22%2C%22generationNumber%22%3A0%2C%22expires%22%3A%222025-01-16T00%3A00%3A00.000Z%22%2C%22signature%22%3A%224f4dbc48cdc6b374bbfabbf76ed184ba98199a8d477be155faf31245759aa32c%22%7D", {
+        "headers": {
+          "accept": "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9",
+          "priority": "u=1, i",
+          "sec-ch-ua": "\"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Linux\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "traceparent": "00-9a610257b783af35181583178af1941f-787c534b9b6893c8-01",
+          "tracestate": "",
+          "x-airtable-application-id": "app05mIKwNPO2l1vT",
+          "x-airtable-client-queue-time": "2.099999999627471",
+          "x-airtable-inter-service-client": "webClient",
+          "x-airtable-inter-service-client-code-version": "0172c9229aae27099ae617d13157821608d409b8",
+          "x-airtable-page-load-id": "pglaBXe7VtCOj5efS",
+          "x-requested-with": "XMLHttpRequest",
+          "x-time-zone": "America/New_York",
+          "x-user-locale": "en"
+        },
+        "referrerPolicy": "no-referrer",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+      }).then(r=>r.json())
+      
+      //@ts-ignore
+    const json =   Object.values(temp1.data.preloadPageQueryResults.tableDataById.tblVV0tpvZnQWcsH4.partialRowById).map(e=>e.cellValuesByColumnId).map(e => {
+          let s = undefined
+          if(e.fld7qAcn49vVKJ4xc) {
+          if(e.fld7qAcn49vVKJ4xc == "selbAbSmkPk9XiuTc") {
+              s = "approved"
+          }
+              if(e.fld7qAcn49vVKJ4xc == "seleUd0UfZzubrPKs") {
+                  s = "being reviewed"
+              }
+          }
+      return {
+      name: e.fldeAMpKMvhiFAokN,
+      url: e.fldiQTbHOJ4Smo2Cx,
+          status: s 
+      }
+      })
+      const myInstance = json.find(e => e.name.includes("Saahil"))
+      const myDbInstance = app.db.get("boba_status") || null 
+      if (!myDbInstance) {
+        await app.client.chat.postMessage({
+          channel: "C07R8DYAZMM",
+          text: `:christmas_tree::snowflake::gift::cup_with_straw: *Winter boba status*: ${myInstance.status || "Non existent"}`
+        })
+      } else if(myDbInstance !== myInstance.status) {
+        await app.client.chat.postMessage({
+          channel: "C07R8DYAZMM",
+          text: `:christmas_tree::snowflake::gift::cup_with_straw: *Winter boba status updated*: ${myInstance.status || "Non existent"}`
+        })
+      }
+    } catch (e:any) {
+      app.client.chat.postMessage({
+        channel: process.env.MY_USER_ID,
+        text: `so the airtable automation broke\n${e.stack || e.message}`
+      })
+    }
+  })
   setupCronForShipments(app);
   startBdayCron(app);
   setupCronAdventOfCode(app);
