6277d45f10de1b06d7cbe6f7d240fdb6c939835c
Saahil <neon@saahild.com>
2025-01-03 22:11:47 -0500

    Update home.ts

--- src/commands/home.ts
+++ src/commands/home.ts
@@ -269,7 +269,7 @@ export default class AppHome implements Command {
                     .slice(0, 4)
                     .map(
                       (e) =>
-                        `${e.icon}${e.isDone ? `:white_check_mark: ` : ":loading:"} -- ${e.content?s.length > 0 ? e.contents.join(", ") : e.shipmentTitle}`,
+                        `${e.icon}${e.isDone ? `:white_check_mark: ` : ":loading:"} -- ${e.contents?.length > 0 ? e.contents.join(", ") : e.shipmentTitle}`,
                     )
                     .join("\n")}`,
                 },
