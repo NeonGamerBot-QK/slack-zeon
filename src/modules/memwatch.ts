df871551fec4bde225fe459d1eb062482c2f5fa7
Saahil <neon@saahild.com>
2025-01-06 12:38:22 -0500

    fix:mem

--- src/modules/memwatch.ts
+++ src/modules/memwatch.ts
@@ -9,9 +9,14 @@ export default function watchMem(app: ModifiedApp) {
     });
   });
   memwatch.on("stats", function (stats) {
-    app.client.chat.postMessage({
-      text: `debug: mem stats ;3 \n\`\`\`\n${JSON.stringify(stats, null, 2)}\n\`\`\``,
-      channel: `C07LGLUTNH2`,
-    });
+    // app.client.chat.postMessage({
+    //   text: `debug: mem stats ;3 \n\`\`\`\n${JSON.stringify(stats, null, 2)}\n\`\`\``,
+    //   channel: `C07LGLUTNH2`,
+    // });
+    // save in db for graph ;p
+    const old = app.db.get(`memwatch`) || [];
+    old.push(stats);
+    if (old.length > 100) old.shift();
+    app.db.set(`memwatch`, old);
   });
 }
