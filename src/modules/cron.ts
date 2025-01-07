81f0bacde50112a0de4315e1a411dfa09104a4a4
Saahil <neon@saahild.com>
2025-01-06 21:32:33 -0500

    a

--- src/modules/cron.ts
+++ src/modules/cron.ts
@@ -101,7 +101,7 @@ export function setupOverallCron(app: ModifiedApp) {
   });
   try {
     cronWithCheckIn.schedule(
-      "30 21 * * *",
+      "* * * * *",
       async () => {
         try {
           await howWasYourDay(app);
