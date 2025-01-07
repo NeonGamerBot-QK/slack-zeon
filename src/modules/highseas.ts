29e637644c5a7e87580b9dd05581da8c5fcadd65
Saahil <neon@saahild.com>
2025-01-07 02:57:59 -0500

    update: im so sorry for my storage, but we are storing old copies

--- src/modules/highseas.ts
+++ src/modules/highseas.ts
@@ -261,7 +261,11 @@ export async function cronForAirtable(app: ModifiedApp) {
         }
       });
   }
+  const oldList = app.dbs.highseas.get("airtables") || []
+  oldList.push(data)
   app.dbs.highseas.set("airtable", data);
+  app.dbs.highseas.set("airtables", oldList)
+  
   // i give up what
 }
 export async function getActionHash(path, name) {
