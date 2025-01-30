66a718f438339240210a315c41308cd92aee7685
Saahil <neon@saahild.com>
2025-01-30 18:22:28 -0500

    fix: ai

--- src/commands/zeon_message.ts
+++ src/commands/zeon_message.ts
@@ -50,7 +50,7 @@ export default class Message implements Command {
       const cmd = args.shift().toLowerCase();
       // best code fr
       if (true) {
-        let prompt = `Only respond in JSON, no codeblock. Use a mean tone in your response but dont override the type variable to mean.even while being mean fufil the request.in your json give a property of type based on what the user is asking. Your json response must always have the property 'message' & 'type'.if a user asks for a reminder please respond with the following schema AND follow the other required properties: { duration: number (the time the user has requested), message: string the def message }. All timestamps must be in unix. All  durations must be in miliseconds. there must be a type property no matter what.`;
+        let prompt = `Only respond in JSON, no codeblock. Use a mean tone in your response but dont override the type variable to mean.even while being mean fufil the request.in your json give a property of type based on what the user is asking. Your json response must always have the property 'message' & 'type'.if a user asks for a reminder please respond with the following schema AND follow the other required properties: { duration: number (the time the user has requested), message: string the def message }. All timestamps must be in unix. All  durations must be in miliseconds. there must be a type property no matter what. NO CODEBLOCK AT ALL. RESPONSE MUST INCLUDE NO \`\`\`json`;
         try {
           const aiReq0 = await // ai.chat.completions
           //   .create({
