99b56d7d2a120650f944ada215ad30a22da3c5ec
Saahil <neon@saahild.com>
2025-02-11 21:09:58 -0500

    fix: more ratelimits

--- src/modules/capslockwhat.ts
+++ src/modules/capslockwhat.ts
@@ -5,14 +5,15 @@ export default function watchWS(app: ModifiedApp) {
   const ws = new WebsocketClient("wss://globalcapslock.com/ws");
   let msgsArray = [];
   const cb = (message: string) =>
-    msgsArray.length < 4
+    msgsArray.length < 12
       ? msgsArray.push(message)
       : app.client.chat.postMessage({
           text: msgsArray.join("\n"),
           channel: "C08CQ8CMR5K",
         });
   let lastSwitch = -1;
-  ws.on("message", (d) => {
+  ws.on("message", async (d) => {
+    await new Promise(r=>setTimeout(r,1000));
     const switc: number = parseFloat(d.toString());
     if (switc == lastSwitch) return;
     cb(`Switched from \`${lastSwitch}\` -> \`${switc}\``);
