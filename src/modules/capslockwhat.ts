7df69f81e2563220a1f08da11ad2094851c5a7b7
Saahil <neon@saahild.com>
2025-02-11 20:43:46 -0500

    light cache

--- src/modules/capslockwhat.ts
+++ src/modules/capslockwhat.ts
@@ -3,8 +3,9 @@ import WebsocketClient from "ws"
 import { ModifiedApp } from "./slackapp";
 export default function watchWS(app: ModifiedApp) {
     const ws = new WebsocketClient("wss://globalcapslock.com/ws")
-const cb = (message: string) => app.client.chat.postMessage({
-    text: message,
+    let msgsArray = []
+const cb = (message: string) => msgsArray.length < 4 ? msgsArray.push(message) : app.client.chat.postMessage({
+    text: msgsArray.join("\n"),
     channel: "C08CQ8CMR5K"
 })
 let lastSwitch = -1;
