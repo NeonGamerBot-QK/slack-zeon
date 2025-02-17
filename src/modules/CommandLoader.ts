7cdf72cde803cd4db80e23db53778f48a9c56133
Saahil <neon@saahild.com>
2025-02-16 20:06:20 -0500

    fix: loader being slow maybe

--- src/modules/CommandLoader.ts
+++ src/modules/CommandLoader.ts
@@ -33,16 +33,39 @@ export default class CommandLoader {
   }
   public async runQuery() {
     const files = this.getFiles();
+    const cmds = []
+    let logging_values = []
     for (const file of files) {
       try {
         const commandClass = require(path.join(this.dir, file));
-        const cmd = new commandClass.default();
-        cmd.run(this._app);
-        console.log(`Loaded ${file}`);
+        cmds.push({ commandClass, file })
+        // const cmd = new commandClass.default();
+        // cmd.run(this._app);
+        console.log(`Loaded ${file}`); 
+        logging_values.push({ file, loaded: true })
       } catch (e) {
         console.error(e);
         console.error(`Failed to load ${file}`);
+        logging_values.push({ file, failed: true })
       }
     }
+    for(const { commandClass, file } of cmds) {
+      const stamp = Date.now()
+      const c = new commandClass.default();
+      console.log(`Running ${file}`)
+      try {
+        c.run(this._app);
+        console.log(`Ran ${file}`)
+        logging_values.find(e => e.file === file).ran = true
+      } catch (e) {
+        console.error(e);
+        console.error(`Failed to run ${file}`);
+        logging_values.find(e => e.file === file).failed_run = true
+      } finally {
+        console.log(`Finished ${file}`)
+        logging_values.find(e => e.file === file).took = `${Date.now() - stamp}ms`
+      }
+    }
+    console.table(logging_values)
   }
 }
