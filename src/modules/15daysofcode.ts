6f0f885443204b35a5cbbf6f12751f9e9d9511ca
Saahil <neon@saahild.com>
2025-02-22 23:13:16 -0500

    fix: data typing

--- src/modules/15daysofcode.ts
+++ src/modules/15daysofcode.ts
@@ -7,9 +7,9 @@ export function parseTextAndExtractUsers(text:string) {
     return text.split('|--------------------')[2].split('\n').filter(Boolean).map(e=>e.split("|").filter(Boolean).map(d=>d.trim().replace('✓ ', ''))).filter(e=>e.length > 1).map(d=> {
         return {
             username: d[0],
-            days: d.slice(1)
+            days: d.slice(1).filter(Boolean)
         }
-        })
+        }).filter(e=>!e.username.startsWith('---'))
 }
 export function getMetaInfo(text:string) {
     const [Rstatus, Rwrote] = text.split('-------------------\nEND OF TRANSMISSION')[1].split('---')[0].split('\n').filter(Boolean)
