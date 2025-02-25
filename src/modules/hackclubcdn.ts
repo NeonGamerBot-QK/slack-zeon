5fca3109003b943b527e4ab083f1349bd3c83b2e
Neon <neon@saahild.com>
2025-02-25 14:46:44 -0700

    fix: new update

--- src/modules/hackclubcdn.ts
+++ src/modules/hackclubcdn.ts
@@ -1,6 +1,10 @@
 export function uploadURL(url: string[]) {
-  return fetch(`https://cdn-2moycvmxo.hackclub.dev/api/v1/new`, {
+  return fetch(`https://cdn.hackclub.com/api/v1/new`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer beans" 
+    }
     body: JSON.stringify(url),
   }).then((r) => r.json());
 }
