3cfee37743cdc3ac6395a2d744d6e5e14fa689e7
Neon <neon@saahild.com>
2025-03-13 21:06:17 -0400

    fix: send correctly

--- src/commands/onuserjoinslack.ts
+++ src/commands/onuserjoinslack.ts
@@ -18,10 +18,43 @@ export default class UserJoinSlackEvent implements Command {
     app.event(this.name, async ({ event, say, body }) => {
       console.debug(event, "#userjoinslack");
       //@ts-ignore
-      app.client.chat.postMessage({
-        text: JSON.stringify(event),
-        channel: "#zeon-public",
-      });
+        app.client.chat.postMessage({
+            blocks: [
+                {
+                  type: "section",
+                  text: {
+                    type: "mrkdwn",
+                    text: `${event.user.name} (${event.user.real_name}) (<@${event.user.id}>) just joined the slack!`,
+                  },
+                  accessory: {
+                    type: "image",
+                    image_url: event.user.profile.image_192 || event.user.profile.image_48,
+                    alt_text:
+                      "Avatar of " +
+                      event.user.name,
+                  },
+                },
+                // {
+                //   type: "divider",
+                // },
+                // {
+                //   type: "actions",
+                //   elements: [
+                //     {
+                //       type: "button",
+                //       text: {
+                //         type: "plain_text",
+                //         text: "Steam User Link",
+                //         emoji: true,
+                //       },
+                //       value: "steam_user_link",
+                //       url: player.profileurl,
+                //     },
+                //   ],
+                // },
+              ],
+            channel: "C08HTF94XV2"
+    })
     });
   }
 }
