d73ee0e8d8c3b4250f2492b08a898b5e59260318
Saahil <neon@saahild.com>
2025-04-24 19:09:43 -0400

    fix: cors

--- src/modules/slackapp.ts
+++ src/modules/slackapp.ts
@@ -135,6 +135,9 @@ export const app = new App({
       path: "/shipwreck-data.json",
       method: ["GET"],
       async handler(req, res) {
+        await new Promise((resolve) => {
+          require("cors")({ origin: "*" })(req, res, resolve);
+        });
         //@ts-ignore
         const data = app.db.get("ship_wrecks_entries");
         res.writeHead(200).end(JSON.stringify(data));
