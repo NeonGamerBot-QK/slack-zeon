19685c8fff93fb1b441fecfff3b748eaf5792b2e
Saahil <neon@saahild.com>
2025-06-26 20:49:50 -0400

    update: ananlitics

--- src/commands/check-verification.ts
+++ src/commands/check-verification.ts
@@ -12,6 +12,7 @@ export default class Ping implements Command {
     app.command(this.name, async ({ command, ack, respond }) => {
       const stamp = Date.now();
       await ack();
+
       const input = command.text.trim() || `<@${command.user_id}>`
     const isSlackId = (input.startsWith("<@") || input.startsWith("U")) && input.length > 10
 const idtStatus = await         fetch("https://identity.hackclub.com/api/external/check"+ (isSlackId ? `?slack_id=${input.replace("<@", "").replace(">", "")}` : `?email=${encodeURIComponent(input)}`)).then(r=>r.json()).then(d=>d.result)
@@ -19,6 +20,20 @@ await respond({
     response_type: "ephemeral",
     text: `*${input}* is *${idtStatus}*`
 })
+      try {
+             await app.logsnag.track({
+        channel: "idv-check",
+        event: "usage",
+        user_id: command.user_id,
+        icon: "📜",
+        description: "checked verification status",
+                tags: {
+                    status: idtStatus
+                }
+      });
+      } catch (e) {
+       console.error('idv stat log error', e) 
+      }
     });
   }
 }
