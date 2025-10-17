1d30415bdb320d7d16e399423de2caf7af8d6224
Neon <neon@saahild.com>
2025-10-16 21:53:30 -0400

    fix: istg this code hates me

--- src/commands/on_message.ts
+++ src/commands/on_message.ts
@@ -166,17 +166,17 @@ export default class Message implements Command {
                 const is_banned = banned_users.includes(user)
                   ? true
                   : await fetch(
-                      `https://hackatime.hackclub.com/api/v1/users/${user}/stats?features=projects&start_date=2025-07-01`,
-                    )
-                      .then((r) => r.json())
-                      .then((d) =>
-                        d.trust_factor
-                          ? d.trust_factor.trust_level == "red"
-                          : false,
-                      );
+                    `https://hackatime.hackclub.com/api/v1/users/${user}/stats?features=projects&start_date=2025-07-01`,
+                  )
+                    .then((r) => r.json())
+                    .then((d) =>
+                      d.trust_factor
+                        ? d.trust_factor.trust_level == "red"
+                        : false,
+                    );
                 console.log(`Checking ${user}`);
                 if (!is_banned) continue;
-                say(`Removing ${user} from ${channel}`);
+                say(`Removing ${user}`);
                 await app.client.conversations.kick({
                   channel: "C07R8DYAZMM",
                   user,
@@ -188,7 +188,7 @@ export default class Message implements Command {
                 });
                 kicked++;
               } catch (e) {
-                console.log(`Error removing ${user} from ${channel}: ${e}`);
+                console.log(`Error removing ${user}: ${e}`);
               }
               await new Promise((r) => setTimeout(r, 700));
             }
