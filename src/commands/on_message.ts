8c1efb999a051f52a510d4fe8bbdf607f1100553
Neon <neon@saahild.com>
2025-10-16 21:49:26 -0400

    fix: channel id

--- src/commands/on_message.ts
+++ src/commands/on_message.ts
@@ -150,7 +150,7 @@ export default class Message implements Command {
             // Keep fetching until no next_cursor
             do {
               const result = await app.client.conversations.members({
-                channel: channelId,
+                channel: "C07R8DYAZMM",
                 limit: 200, // max allowed
                 cursor: cursor,
               });
@@ -165,19 +165,19 @@ export default class Message implements Command {
                 const is_banned = banned_users.includes(user)
                   ? true
                   : await fetch(
-                      `https://hackatime.hackclub.com/api/v1/users/${user}/stats?features=projects&start_date=2025-07-01`,
-                    )
-                      .then((r) => r.json())
-                      .then((d) =>
-                        d.trust_factor
-                          ? d.trust_factor.trust_level == "red"
-                          : false,
-                      );
+                    `https://hackatime.hackclub.com/api/v1/users/${user}/stats?features=projects&start_date=2025-07-01`,
+                  )
+                    .then((r) => r.json())
+                    .then((d) =>
+                      d.trust_factor
+                        ? d.trust_factor.trust_level == "red"
+                        : false,
+                    );
                 console.log(`Checking ${user}`);
                 if (!is_banned) continue;
                 say(`Removing ${user} from ${channel}`);
                 await app.client.conversations.kick({
-                  channel,
+                  channel: "C07R8DYAZMM",
                   user,
                 });
 
